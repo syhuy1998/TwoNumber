22ed00bffbea405961e945915b5e9840a6edd712
syhuy1998 <43946763+syhuy1998@users.noreply.github.com>
2018-12-21 10:25:01 +0700

    Update MyBigNumber.java

--- MyBigNumber.java
+++ MyBigNumber.java
@@ -1,47 +1,93 @@
 /**
-* Class MyBigNumber is the class to sum the 2 number with 2 number know as string
-* Class sum is that to sum two number
-*/
+ * Sum two string class.
+ */
+public class MyBigNumber {
 
-public class MyBigNumber { 
+    private IReceiver receiver;
+
+    public MyBigNumber(){}
+
+    public MyBigNumber(final IReceiver receiver) {
+        this.receiver = receiver;
+    }
+
 
     /**
-    * 2 string only contain number from 0 to 9
-    * @param s1 is the first string
-    * @param s2 is the second string
-    * @return the result is sum of the s1 and s2
-    */
-    
-    public String sum(final String s1, final String s2) { 
-        String result = "";// the result value
-        int str1 = s1.length(); // lenght of the 1st string
-        int str2 = s2.length();	// lenght of the 2nd string 
+     * Sum two string function.
+     */
 
-        int max = (str1 > str2) ? str1 : str2;// Take the longest length of the 1st and 2nd string
-        int sur = 0;// value save the projected value
-    	char digit1 = '0'; // the value save the character is taken
-        char digit2 = '0'; //
-        int pos1;// the last postion of 1st string
-        int pos2; // the last position of the 2nd string
-    
-        int tong = 0;// the value to sum the last digit of 2 number
-        
-        for (int i = 0; i < max; i++) { 
-            pos1 = str1 - i - 1;// update the position of the number
-            pos2 = str2 - i - 1; 
-            
-            if (pos1 >= 0) { 
-                digit1 = s1.charAt(pos1); // take the last digit of the number
-            } 
-            if (pos2 >= 0) { 
-                digit2 = s2.charAt(pos2); 
-            } 
+    public String sum( String s1,  String s2) {
 
-            tong = (digit1 - '0') + (digit2 - '0') + nho;// turn sting into number and then sum with the projected value 
-            result += (tong % 10); // result'Take the value Tong / 10 and then sum into value result.
-            sur = tong / 10;// update the projected number.
+        String result = "";             // chuỗi chứa kết quả
+        String stepMsg = "";         // chuỗi chứa các bước trong quá trình cộng
+
+        int temp = 0;                // biến lấy tổng
+        int temp2 = 0;               // biến lấy hàng đơn vị
+        int carry = 0;               // biến nhớ
+        int i = 0;                   // biến đếm
+
+        int n1 = 0;                 // biến chứa số trong chuỗi 1
+        int n2 = 0;                  // biến chứa số trong chuỗi 2
+
+        int len1 = s1.length();      // biến chứa độ dài chuỗi 1
+        int len2 = s2.length();      // biến chứa độ dài chuỗi 2
+
+
+        int length = len1 < len2 ? len2 : len1; // biến chứa độ dài lớn nhất trong 2 chuỗi s1, s2
+
+        if (s1.contains("-")) {
+            throw new NumberFormatException("So thu nhat phai la so nguyen duong");
         }
-        
+
+        if (s2.contains("-")) {
+            throw new NumberFormatException("So thu nhi phai la so nguyen duong");
+        }
+
+        for (i = 0; i < length; i++) {
+
+            n1 = i < len1 ? (s1.charAt(len1 - i - 1) - '0') : 0;
+            n2 = i < len2 ? (s2.charAt(len2 - i - 1) - '0') : 0;
+
+            temp = n2 + n1 + carry;
+            temp2 = temp % 10;
+
+            if (carry == 0) {
+                stepMsg += "\n" + "Buoc " + (i + 1) + ": "
+                        + "lay " + n1
+                        + ", cong " + n2
+                        + ", bang " + temp
+                        + ", viet " + temp2
+                        + ", nho " + temp / 10 + "\n";
+            } else {
+                stepMsg += "\n" + "Buoc " + (i + 1) + ": "
+                        + "lay " + n1
+                        + ", cong " + n2
+                        + ", nho " + carry
+                        + ", bang " + temp
+                        + ", viet " + temp2
+                        + ", nho " + carry + "\n";
+            }
+
+            carry = temp / 10;
+            result = temp2 + result;
+
+        }
+
+        if (carry > 0) {
+            result = carry + result;
+            stepMsg += "\n" + "Buoc " + (i + 1) + ": "
+                    + "lay " + 0
+                    + ", cong " + 0
+                    + ", nho " + 1
+                    + ", bang " + 1
+                    + ", viet " + 1
+                    + "\n";
+        }
+
+
+        receiver.sendMessage(stepMsg);
+
         return result;
+
     }
 }
